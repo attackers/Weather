23f2d4b54b9a3b5da7612ddf2e6f5ff17040ed6d
lifu <305296172@qq.com>
2019-03-27 21:54:24 +0800

    添加接口Key

--- Weather/Header.swift
+++ Weather/Header.swift
@@ -9,7 +9,9 @@
 import Foundation
 import UIKit
 let wenther1 = "http://www.weather.com.cn/m/i/icon_weather/42x30/"
-let juheKey = ""
+let juheWeatherKey = "39b0d147efe0f6cbe7c8b88f5bf030a1"
+let juheHistoryKey = "6e91d3944b9a1f6816706bd8e06acc9e"
+
 /// UIScreen宽度
 let SCREENWIDTH = UIScreen.main.bounds.width
 /// UIScreen高度
